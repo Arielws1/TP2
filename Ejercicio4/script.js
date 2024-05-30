472a331fe6e5cf0b45cddf5100b7b20ac3019ad5
Ariel Perez <arielperez012@gmail.com>
2024-05-30 15:33:34 -0300

    arreglo del ejercicio 4

--- Ejercicio4/script.js
+++ Ejercicio4/script.js
@@ -1,14 +1,6 @@
-document.getElementById('imcForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    // Obtener los valores de peso y estatura
-    const peso = parseFloat(document.getElementById('peso').value);
-    const estatura = parseFloat(document.getElementById('estatura').value);
-
-    // Calcular el IMC
-    const imc = peso / (estatura * estatura);
-
-    // Mostrar el resultado
-    const resultadoDiv = document.getElementById('resultado');
-    resultadoDiv.textContent = `Tu IMC es: ${imc.toFixed(2)}`;
-});
+document.getElementById("clickMe").onclick = test;
+function test(){
+    var h=document.getElementById("h").value;
+    var m=document.getElementById("m").value;
+    alert(m/(h*0.01*h*0.01));
+}   
