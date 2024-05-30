a3aa58ec931b0e90f826d2cfea5a47c34e29f66c
Ariel Perez <arielperez012@gmail.com>
2024-05-30 18:08:17 -0300

    arreglo de metros

--- Ejercicio4/script.js
+++ Ejercicio4/script.js
@@ -2,5 +2,7 @@ document.getElementById("clickMe").onclick = test;
 function test(){
     var h=document.getElementById("h").value;
     var m=document.getElementById("m").value;
-    alert(m/(h*0.01*h*0.01));
-}   
+    var resultado = m/(h*h);
+    var mostrar = resultado.toFixed(1);
+    alert(mostrar);
+}
